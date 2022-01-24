1f3a1989f3eb251a831a7e4f336b79f6d9a18b5c
dantran16 <dantranux@gmail.com>
2022-01-24 15:34:35 -0800

    Icons, Favorites, and Comments

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
-import Home from './HomeComponent'
-import Directory from './DirectoryComponent'
-import CampsiteInfo from './CampsiteInfoComponent'
 import { View, Platform } from 'react-native'
 import Constants from 'expo-constants'
 import { createStackNavigator } from 'react-navigation-stack'
 import { createDrawerNavigator } from 'react-navigation-drawer'
 import { createAppContainer } from 'react-navigation'
+
+import Home from './HomeComponent'
+import Directory from './DirectoryComponent'
+import CampsiteInfo from './CampsiteInfoComponent'
+import About from './AboutComponent'
+import Contact from './ContactComponent'
 
 const DirectoryNavigator = createStackNavigator(
   {
@@ -44,10 +47,46 @@ const HomeNavigator = createStackNavigator(
   }
 )
 
+const AboutNavigator = createStackNavigator(
+  {
+    About: { screen: About }
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: '#5637DD'
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        color: '#fff'
+      }
+    }
+  }
+)
+
+const ContactNavigator = createStackNavigator(
+  {
+    Contact: { screen: Contact }
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: '#5637DD'
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        color: '#fff'
+      }
+    }
+  }
+)
+
 const MainNavigator = createDrawerNavigator(
   {
     Home: { screen: HomeNavigator },
-    Directory: { screen: DirectoryNavigator }
+    Directory: { screen: DirectoryNavigator },
+    About: { screen: AboutNavigator },
+    Contact: { screen: ContactNavigator }
   },
   {
     drawerBackgroundColor: '#CEC8FF'
