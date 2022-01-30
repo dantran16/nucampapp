5807de7b4d78bcf4d8ac4b549804e7e40f1a1d88
dantran16 <dantranux@gmail.com>
2022-01-30 15:47:21 -0800

    Favorites

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -27,6 +27,7 @@ import CampsiteInfo from "./CampsiteInfoComponent";
 import About from "./AboutComponent";
 import Contact from "./ContactComponent";
 import Reservation from "./ReservationComponent";
+import Favorites from './FavoritesComponent'
 
 const mapDispatchToProps = {
 	fetchCampsites,
@@ -166,6 +167,31 @@ const ReservationNavigator = createStackNavigator(
 	}
 );
 
+const FavoritesNavigator = createStackNavigator(
+	{
+		Favorites: { screen: Favorites },
+	},
+	{
+		defaultNavigationOptions: ({ navigation }) => ({
+			headerStyle: {
+				backgroundColor: "#5636DD",
+			},
+			headerTintColor: "#fff",
+			headerTitleStyle: {
+				color: "#fff",
+			},
+			headerLeft: (
+				<Icon
+					name="heart"
+					type="font-awesome"
+					iconStyle={styles.stackIcon}
+					onPress={() => navigation.toggleDrawer()}
+				/>
+			),
+		}),
+	}
+);
+
 const CustomDrawerContentComponent = (props) => (
 	<ScrollView>
 		<SafeAreaView
@@ -212,6 +238,15 @@ const MainNavigator = createDrawerNavigator(
 				drawerLabel: "Reserve Campsite",
 				drawerIcon: ({ tintColor }) => (
 					<Icon name="tree" type="font-awesome" size={24} color={tintColor} />
+				),
+			},
+    },
+    Favorites: {
+			screen: FavoritesNavigator,
+			navigationOptions: {
+				drawerLabel: "My Favorites",
+				drawerIcon: ({ tintColor }) => (
+					<Icon name="heart" type="font-awesome" size={24} color={tintColor} />
 				),
 			},
 		},
