2f0ded4694bd0fc251067805dc0e0fe04b320d4f
dantran16 <dantranux@gmail.com>
2022-01-30 18:18:53 -0800

    Animatable

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,6 +3,7 @@ import { ScrollView, Text, FlatList } from "react-native";
 import { Card, ListItem } from "react-native-elements";
 import { connect } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
+import * as Animatable from "react-native-animatable";
 
 import Loading from "./LoadingComponent";
 
@@ -26,8 +27,8 @@ const Mission = () => {
 				resources.The expert wilderness trekkers on our staff personally verify
 				each campsite to make sure that they are up to our standards.We also
 				present a platform for campers to share reviews on campsites they have
-				visited with each other.{" "}
-			</Text>{" "}
+				visited with each other.
+			</Text>
 		</Card>
 	);
 };
@@ -66,24 +67,28 @@ class About extends Component {
 		if (this.props.partners.errMess) {
 			return (
 				<ScrollView>
-					<Mission />
-					<Card title="Community Partners">
-						<Text> {this.props.partners.errMess} </Text>{" "}
-					</Card>{" "}
+					<Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+						<Mission />
+						<Card title="Community Partners">
+							<Text> {this.props.partners.errMess} </Text>
+						</Card>
+					</Animatable.View>
 				</ScrollView>
 			);
 		}
 
 		return (
 			<ScrollView>
-				<Mission />
-				<Card title="Community Partners">
-					<FlatList
-						data={this.props.partners.partners}
-						renderItem={renderPartner}
-						keyExtractor={(item) => item.id.toString()}
-					/>{" "}
-				</Card>{" "}
+				<Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+					<Mission />
+					<Card title="Community Partners">
+						<FlatList
+							data={this.props.partners.partners}
+							renderItem={renderPartner}
+							keyExtractor={(item) => item.id.toString()}
+						/>
+					</Card>
+				</Animatable.View>
 			</ScrollView>
 		);
 	}
