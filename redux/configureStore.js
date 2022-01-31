3536c4cf4568f54c87ba651195711c503b0d3040
dantran16 <dantranux@gmail.com>
2022-01-31 15:57:14 -0800

    Persist Redux Store

--- redux/configureStore.js
+++ redux/configureStore.js
@@ -1,23 +1,33 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
+import { persistStore, persistCombineReducers } from "redux-persist";
+import storage from "redux-persist/es/storage";
 import { campsites } from "./campsites";
 import { comments } from "./comments";
 import { promotions } from "./promotions";
 import { partners } from "./partners";
-import { favorites } from './favorites'
+import { favorites } from "./favorites";
+
+const config = {
+	key: "root",
+	storage,
+	debug: true,
+};
 
 export const ConfigureStore = () => {
 	const store = createStore(
-		combineReducers({
+		persistCombineReducers(config, {
 			campsites,
 			comments,
 			partners,
-      promotions,
-      favorites
+			promotions,
+			favorites,
 		}),
 		applyMiddleware(thunk, logger)
 	);
 
-	return store;
+	const persistor = persistStore(store);
+
+	return { persistor, store };
 };
